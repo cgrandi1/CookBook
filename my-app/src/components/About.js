0ecfc46ccfaa01afb7350f901b7c55fc0a9b4518
github username <github email address>
2020-01-07 17:39:03 -0700

    recipe

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="about">
       <h1>Thanks For Joining</h1>
-      <h3>Here you are able to any recipe you want. You're able to edit and delete any recipes of your choice. 
+      <h3>Here you are able to create any recipe you want. You're able to edit and delete any recipe of your choice. 
           It's a pesonal cookbook that you have the pleasure of producing. ENJOY!! </h3>
     </div>
   )
