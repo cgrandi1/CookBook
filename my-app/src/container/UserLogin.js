66e04db9e6ae414b35074da385ca0b76cc53bae2
github username <carltondgrandison@gmail.com>
2020-07-03 00:11:35 -0600

    user reducer

--- my-app/src/container/UserLogin.js
+++ my-app/src/container/UserLogin.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {fetchUser} from './action/fetchUser'
 
 class App extends React.Component{
 
-    
+]    
+    render(){
+        return(
+
+
+
+        )
+    }
+
+
 }
+
+
+export default connect(null, {fetchUser})(App); //we just want to fetch our users
--- my-app/src/reducers/rootReducer.js
+++ my-app/src/reducers/rootReducer.js
@@ -1,7 +1,8 @@
 import recipeReducer from './recipeReducer'
 import {combineReducers} from 'redux'
+import { useReducer } from 'react';
 
 
-const rootReducer = combineReducers({recipes: recipeReducer})
+const rootReducer = combineReducers({recipes: recipeReducer}, {users: useRreducer}, {categories: categoryReducer})
 
 export default rootReducer;
